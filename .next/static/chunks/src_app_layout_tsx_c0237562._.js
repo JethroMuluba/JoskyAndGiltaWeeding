408b185f8786794398ef0680a6c3f9f669d0fd48
Jethro Muluba <jethro.muluba@gmail.com>
2025-07-12 03:40:46 +0100

    Remove deprecated files and optimize build manifests: delete unused chunks, transform files, and update server and static assets for improved performance and maintainability.

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__4367d563._.css",
-  "static/chunks/node_modules_183bfbb6._.js",
-  "static/chunks/src_components_ScrollToTop_tsx_54d6ce92._.js"
-],
-    source: "dynamic"
-});
