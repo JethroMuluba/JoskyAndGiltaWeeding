214a3d647a3cba1260381c0a41ea167829c577de
Jethro Muluba <jethro.muluba@gmail.com>
2025-07-12 04:14:32 +0100

    Update favicon handling and optimize image assets: replace favicon files with updated versions, adjust image dimensions for better display, and remove obsolete favicon PNG file to streamline asset management.

---
+++ .next/static/chunks/src_app_guest_[slug]_page_tsx_87f8b788._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d52fc2aa._.js",
+  "static/chunks/src_2be875a1._.js"
+],
+    source: "dynamic"
+});
---
+++ .next/static/chunks/src_app_guest_page_tsx_87f8b788._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_8c68ac09._.js"
+],
+    source: "dynamic"
+});
